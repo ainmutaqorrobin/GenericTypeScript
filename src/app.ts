7c6961425952a11432237dacb65aba129b39019b
Ain Mutaqorrobin <robin@mesiniaga.com.my>
2024-03-01 09:54:30 +0800

    code 9:54 A.M

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-// Code goes here!
+const names: Array<string | number> = [`Robin`, `Ain`];
+
+const promise: Promise<any> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10);
+  }, 2000);
+});
+promise.then((data) => {
+  data.split(` `);
+});
