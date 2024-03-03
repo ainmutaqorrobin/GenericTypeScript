3019334a5adf5d0374b1c1aca907a0de15a72a0a
mutaqorrobin <ainmutaqorrobin@gmail.com>
2024-03-03 21:26:11 +0800

    finalize code

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 function countDescription(element) {
     var descrition = "got no value";
     if (element.length === 1) {
@@ -9,3 +18,34 @@ function countDescription(element) {
     return [element, descrition];
 }
 console.log(countDescription(["Coooking", "STLOO WOO"]));
+function extractAndConvert(obj, key) {
+    return "Value of " + obj[key];
+}
+extractAndConvert({ name: "robin" }, "name");
+var DataStorage = /** @class */ (function () {
+    function DataStorage() {
+        this.data = [];
+    }
+    DataStorage.prototype.addItem = function (item) {
+        this.data.push(item);
+    };
+    DataStorage.prototype.removeItem = function (item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    };
+    DataStorage.prototype.getItems = function () {
+        return __spreadArray([], this.data, true);
+    };
+    return DataStorage;
+}());
+var textStorage = new DataStorage();
+textStorage.addItem("NOKIA");
+textStorage.addItem("PATAYA");
+textStorage.removeItem("NOKIA");
+console.log(textStorage.getItems());
+var item = { name: "Robin", age: 21 };
+var numberStorage = new DataStorage();
+var objStorage = new DataStorage();
+objStorage.addItem(item);
+objStorage.addItem({ name: "Hapiz soib", age: 24 });
+objStorage.removeItem(item);
+console.log(objStorage.getItems());
--- src/app.ts
+++ src/app.ts
@@ -13,3 +13,60 @@ function countDescription<T extends Lengthy>(element: T): [T, string] {
 }
 
 console.log(countDescription([`Coooking`, `STLOO WOO`]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return `Value of ` + obj[key];
+}
+
+extractAndConvert({ name: `robin` }, `name`);
+
+class DataStorage<T extends string | number | object> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  getItems() {
+    return [...this.data];
+  }
+}
+const textStorage = new DataStorage<string>();
+textStorage.addItem(`NOKIA`);
+textStorage.addItem(`PATAYA`);
+textStorage.removeItem(`NOKIA`);
+console.log(textStorage.getItems());
+
+const item = { name: `Robin`, age: 21 };
+const numberStorage = new DataStorage<number>();
+const objStorage = new DataStorage<object>();
+
+objStorage.addItem(item);
+objStorage.addItem({ name: `Hapiz soib`, age: 24 });
+objStorage.removeItem(item);
+console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  name: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = name;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = [`Robin`, `Hafiz soib`];
